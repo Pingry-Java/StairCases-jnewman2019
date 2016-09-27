f13f95fc67b06cb7c8182ca6a810d9750fb80b2c
Jeremy Newman <jnewman2019@pingry.org>
2016-09-27 08:20:08 -0400

    Wrote my code for for StairCases program.

--- StairCases.java
+++ StairCases.java
@@ -1,33 +1,60 @@
 /**
- * Write javadoc for your class here (and remove this comment, or course)
- *
+ * This class will print out two different sets of 'x's, one ascending, and one descending
+ * Each set will print the number of rows taken from the user
+ * @author Jeremy Newman
+ * @version 0.1
  */
+import java.util.Scanner;
 
 public class StairCases
 {
   /**
-   * Main method demonstrates the other wo methods by calling each of them.
+   * Main method demonstrates the other two methods by calling each of them.
    * @param args Standard array of commandline String arguments. Not used in this program.
    */
 
   public static void main(String[] args)
   {
-    //TODO change the next line so it actually gets input from the user
-    int size = 4;
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("How many lines do you want the staircase to be?");
+    int size = Integer.parseInt(keyboard.nextLine());
 
     printAscending(size);
     printDescending(size);
   }
 
   /**
-   * Write your javadoc here
+   * This method prints x's in an ascending triangle
+   * @param size User input representing the number of rows of the triangle printed
    */
-  public static void printAscending
-  //TODO Finish this method
+  public static void printAscending(int size)
+  {
+  	for (int r = 0; r < size; r++)
+  	{
+  		for (int s = 0; s <= r; s++)
+  		{
+  			System.out.print('x');
+  		}
+  		System.out.println();
+  	}
+  }
+  	
+  
+  
 
   /**
-   * Write your javadoc here
+   * This method prints x's in an ascending triangle
+   * @param size User input representing the number of rows of the triangle printed
    */
-  public static void printDescending
-  //TODO Finish this method
+  public static void printDescending(int size)
+  {
+  	for (int r = 0; r < size; r++)
+  	{
+  		for (int s = size; s > r; s--)
+  		{
+  		System.out.print('x');
+  		}
+  		System.out.println();
+  	}
+  }
 }
